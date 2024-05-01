c407dd59d29d01d3cdad775c9dcaaba2bbe0079e
Joons <elddy0948@gmail.com>
2024-05-02 01:27:24 +0900

    init atmega project

---
+++ atmega128_project/atmega128_project/main.c
@@ -0,0 +1,18 @@
+/*
+ * atmega128_project.c
+ *
+ * Created: 2024-05-02 오전 1:25:14
+ * Author : HoJoon
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

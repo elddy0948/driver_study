78b0c98d41ca0aa9e54159e2a600893074e07a0a
Joons <elddy0948@gmail.com>
2024-05-02 23:48:30 +0900

    add function interface

--- nucleo-f767zi/Communication_Practice/Drivers/Inc/stm32f767xx_spi.h
+++ nucleo-f767zi/Communication_Practice/Drivers/Inc/stm32f767xx_spi.h
@@ -25,4 +25,20 @@ typedef struct
 	SPI_Config_t SPIConfig;
 } SPI_Handle_t;
 
+/* Peripheral Clock setup */
+void SPI_PeripheralClockControl(SPI_RegDef_t* pSPIx, uint8_t EnorDi);
+
+/* Init and De-init */
+void SPI_Init(SPI_Handle_t* pSPIHandle);
+void SPI_DeInit(SPI_RegDef_t* pSPIx);
+
+/* Data send and receive */
+void SPI_SendData(SPI_RegDef_t* pSPIx, uint8_t* pTxBuffer, uint32_t length);
+void SPI_ReceiveData(SPI_RegDef_t* pSPIx, uint8_t* pRxBuffer, uint32_t length);
+
+/* IRQ Configuration and ISR handling */
+void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
+void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
+void SPI_IRQHandling(SPI_Handle_t* pHandle);
+
 #endif /* INC_STM32F767XX_SPI_H_ */

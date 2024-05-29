5fb15a151865386e4e69d6c3494ae6e564ba66b8
Joons <elddy0948@gmail.com>
2024-05-29 16:58:54 +0900

    feat : i2c pclk control, peripheral control

--- nucleo-f767zi/Communication_Practice/Drivers/Src/stm32f767xx_i2c.c
+++ nucleo-f767zi/Communication_Practice/Drivers/Src/stm32f767xx_i2c.c
@@ -6,3 +6,37 @@
  */
 
 #include "stm32f767xx_i2c.h"
+
+void I2C_Peripheral_clock_control(I2C_RegDef_t* pI2Cx, uint8_t EnOrDi)
+{
+	if (EnOrDi == ENABLE) {
+		if (pI2Cx == I2C1) {
+			I2C1_PCLK_EN();
+		} else if (pI2Cx == I2C2) {
+			I2C2_PCLK_EN();
+		} else if (pI2Cx == I2C3) {
+			I2C3_PCLK_EN();
+		} else if (pI2Cx == I2C4) {
+			I2C4_PCLK_EN();
+		}
+	} else {
+		if (pI2Cx == I2C1) {
+			I2C1_PCLK_DI();
+		} else if (pI2Cx == I2C2) {
+			I2C2_PCLK_DI();
+		} else if (pI2Cx == I2C3) {
+			I2C3_PCLK_DI();
+		} else if (pI2Cx == I2C4) {
+			I2C4_PCLK_DI();
+		}
+	}
+}
+
+void I2C_Peripheral_control(I2C_RegDef_t* pI2Cx, uint8_t EnOrDi)
+{
+	if (EnOrDi == ENABLE) {
+		pI2Cx->CR1 |= (1 << I2C_CR1_PE);
+	} else {
+		pI2Cx->CR1 &= ~(1 << I2C_CR1_PE);
+	}
+}

01cb144f80e58e210f19a8111c0e0c3d92fa13b7
Joons <elddy0948@gmail.com>
2024-05-29 15:48:35 +0900

    feat : i2c config, handle structure

--- nucleo-f767zi/Communication_Practice/Drivers/Inc/stm32f767xx_i2c.h
+++ nucleo-f767zi/Communication_Practice/Drivers/Inc/stm32f767xx_i2c.h
@@ -8,6 +8,16 @@
 #ifndef INC_STM32F767XX_I2C_H_
 #define INC_STM32F767XX_I2C_H_
 
+typedef struct {
+	uint32_t SCLSpeed;
+	uint8_t DeviceAddress;
+	uint8_t ACKControl;
+	uint16_t FMDutyCycle;
+} I2C_Config_t;
 
+typedef struct {
+	I2C_RegDef_t* pI2Cx;
+	I2C_Config_t I2CConfig;
+} I2C_Handle_t;
 
 #endif /* INC_STM32F767XX_I2C_H_ */

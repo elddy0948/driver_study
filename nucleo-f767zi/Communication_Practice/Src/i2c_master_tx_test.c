cc6ff6fcedef49a9e120aec9c68c22713e8f90ef
Joons <elddy0948@gmail.com>
2024-06-04 00:36:14 +0900

    feat : i2c1 initialize

--- nucleo-f767zi/Communication_Practice/Src/i2c_master_tx_test.c
+++ nucleo-f767zi/Communication_Practice/Src/i2c_master_tx_test.c
@@ -7,6 +7,8 @@
 
 #include "stm32f767xx.h"
 
+I2C_Handle_t i2c1_handle;
+
 /**
  * ALTERNATE FUNCTION 4
  * I2C1
@@ -29,4 +31,21 @@ void I2C1_GPIO_initialize(void)
 
 	i2c_pins.PinConfig.PinNumber = GPIO_PIN_NUMBER_7;
 	GPIO_Init(&i2c_pins);
+}
+
+void I2C1_initialize(void)
+{
+	i2c1_handle.pI2Cx = I2C1;
+	i2c1_handle.I2CConfig.ACKControl = I2C_ACK_ENABLE;
+	i2c1_handle.I2CConfig.DeviceAddress = 0x61;
+	i2c1_handle.I2CConfig.SCLSpeed = I2C_SCL_SPEED_STANDARD;
+	I2C_Initialize(&i2c1_handle);
+}
+
+int main(void)
+{
+	I2C1_GPIO_initialize();
+	I2C1_initialize();
+
+	return 0;
 }
